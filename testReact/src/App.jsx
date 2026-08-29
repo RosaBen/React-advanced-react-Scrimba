aef61fa054dd9da171b7ddfa24ccd6574ed94760
RosaBen <benchabane.rosa@outlook.com>
2026-08-29 23:28:34 +0200

    deleted package

--- testReact/src/App.jsx
+++ testReact/src/App.jsx
@@ -1,7 +1,20 @@
+// packages
+
 // Components
+import Button from "./components/Button";
+import Marquee from "./components/Marquee";
+
+// Styles
+import "./assets/styles/button.css";
+import "./assets/styles/marquee.css";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <Marquee text="🧛‍♀️ Welcome to Horrorville 🧛‍♀️" />
+      <Button text="Click here" />
+    </main>
+  );
 }
 
 export default App;
---
+++ testReact/src/components/Marquee.jsx
@@ -0,0 +1,7 @@
+export default function Marquee({ text }) {
+  return (
+    <div className="marquee">
+      <h1>{text}</h1>
+    </div>
+  );
+}
