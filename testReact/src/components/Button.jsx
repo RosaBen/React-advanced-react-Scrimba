38d72527b762d2424306eec6ff1508cf9887610b
RosaBen <benchabane.rosa@outlook.com>
2026-08-30 02:05:54 +0200

    destructuring props

--- testReact/src/components/Button.jsx
+++ testReact/src/components/Button.jsx
@@ -1,10 +1,12 @@
 import clsx from "clsx";
 
 export default function Button({ children, size, className, ...rest }) {
-  const classBtn =
-    size === "sm" ? `button-small ${className}` : `button-large ${className}`;
+  let classBtn;
+
+  size === "sm" ? (classBtn = `button-small`) : (classBtn = `button-large`);
+  const allCasses = clsx(classBtn, className);
   return (
-    <button className={classBtn} {...rest}>
+    <button className={allCasses} {...rest}>
       {children}
     </button>
   );
