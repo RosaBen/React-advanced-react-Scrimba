08e52e782c40d21687b77276ab7fc02ee9d7d287
RosaBen <benchabane.rosa@outlook.com>
2026-08-30 23:36:52 +0200

    working on React Children

--- testReact/src/components/MenuBtn.jsx
+++ testReact/src/components/MenuBtn.jsx
@@ -1,5 +1,5 @@
 import Button from "./Button";
 
-export default function MenuBtn({ children, toggle }) {
+export default function MenuBtn({ children, toggle, open }) {
   return <Button onClick={toggle}>{children}</Button>;
 }
--- testReact/src/components/MenuDropdown.jsx
+++ testReact/src/components/MenuDropdown.jsx
@@ -1,3 +1,14 @@
-export default function MenuDropdown({ children, open }) {
-  return open && <div className="menu-dropdown">{children}</div>;
+export default function MenuDropdown({ children, open, toggle }) {
+  return (
+    open && (
+      <div className="menu-dropdown">
+        {React.Children.map(children, (child) => {
+          return React.cloneElement(child, {
+            open,
+            toggle,
+          });
+        })}
+      </div>
+    )
+  );
 }
