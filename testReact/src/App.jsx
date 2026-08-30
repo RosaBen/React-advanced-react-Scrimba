5c731c327cd45ec456d3eb050fb73a838cf2c9c6
RosaBen <benchabane.rosa@outlook.com>
2026-08-30 20:23:11 +0200

    starting with prop drilling

--- testReact/src/App.jsx
+++ testReact/src/App.jsx
@@ -1,53 +1,13 @@
-// packages
-import { FaMoneyBill } from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
-import { IoPerson } from "react-icons/io5";
-
-// Components
-import Button from "./components/Button";
-import Marquee from "./components/Marquee";
-import Avatar from "./components/Avatar";
-
-// images
-import avatarImg from "./assets/images/avatarRB.jpg";
 // Styles
-import "./assets/styles/button.css";
+// import "./assets/styles/button.css";
 import "./assets/styles/marquee.css";
 import "./assets/styles/avatar.css";
+import "./assets/styles/menu.css";
+
+import Main from "./components/Main";
 
 function App() {
-  return (
-    <main>
-      <Avatar src={avatarImg} alt="Rosa Ben" />
-      <br />
-      <Avatar>RB</Avatar>
-      <br />
-      <Avatar />
-      {/* <Marquee>"🧛‍♀️ Welcome to Horrorville 🧛‍♀️"</Marquee> */}
-      {/* <Button>{<FaMoneyBill />} Buy now!</Button> */}
-      {/* <Button
-        variant
-        style={{ color: "green" }}
-        onClick={() => console.log("Logging in...")}
-      > */}
-      {/* <Button size="sm" className="green">
-        <FcGoogle />
-        Log in with Google
-      </Button>
-      <Button size="lg" variant="success">
-        <FcGoogle />
-        Log in with Google
-      </Button>
-      <Button size="sm" variant="danger">
-        <FcGoogle />
-        Log in with Google
-      </Button>
-      <Button size="lg" variant="warning">
-        <FcGoogle />
-        Log in with Google
-      </Button> */}
-    </main>
-  );
+  return <></>;
 }
 
 export default App;
--- testReact/src/components/Avatar.jsx
+++ testReact/src/components/Avatar.jsx
@@ -2,7 +2,7 @@ import { IoPerson } from "react-icons/io5";
 import { avatarColor } from "../assets/scripts/data";
 import { generateRandomColor } from "../assets/scripts/utils";
 
-export default function Avatar({ variant, src, alt, children, ...rest }) {
+export default function Avatar({ src, alt, children }) {
   const color = avatarColor[generateRandomColor(avatarColor)];
   const style = { backgroundColor: color };
   if (src) {
