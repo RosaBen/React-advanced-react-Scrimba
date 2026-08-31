4083cf491d79b89ad2b4e8143b1b21bdb0b0e1eb
RosaBen <benchabane.rosa@outlook.com>
2026-09-01 00:34:52 +0200

    refactor App

--- test3/src/components/Menu/Menu.jsx
+++ test3/src/components/Menu/Menu.jsx
@@ -1,5 +1,21 @@
-import { createContext } from "react";
+import { Toggle } from "../../assets/scripts/index";
 
 export default function Menu({ children }) {
-  return <div className="menu">{children}</div>;
+  return (
+    <Toggle>
+      <div className="menu">
+        <Toggle.Button>
+          <Menu.Btn>Menu</Menu.Btn>
+        </Toggle.Button>
+        <Toggle.On>
+          <Menu.Dropdown>
+            <Menu.Item>Home</Menu.Item>
+            <Menu.Item>Contact</Menu.Item>
+            <Menu.Item>Projects</Menu.Item>
+            <Menu.Item>Profile</Menu.Item>
+          </Menu.Dropdown>
+        </Toggle.On>
+      </div>
+    </Toggle>
+  );
 }
