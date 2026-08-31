ccc952223fe87ffea6a7470e9aa750d3f5ce5a3c
RosaBen <benchabane.rosa@outlook.com>
2026-08-31 15:59:45 +0200

    working on toggle btn with test3

---
+++ test3/src/App.jsx
@@ -0,0 +1,12 @@
+import Star from "./components/Star";
+import Toggle from "./components/Toggle";
+
+function App() {
+  return (
+    <Toggle>
+      <Star />
+    </Toggle>
+  );
+}
+
+export default App;
---
+++ test3/src/components/Toggle.jsx
@@ -0,0 +1,16 @@
+import { useState, createContext } from "react";
+
+const ToggleContext = createContext();
+export default function Toggle({ children }) {
+  const [on, setOn] = useState(false);
+  function toggle() {
+    setOn((prev) => !prev);
+  }
+  return (
+    <ToggleContext.Provider value={{ on, toggle }}>
+      {children}
+    </ToggleContext.Provider>
+  );
+}
+
+export { ToggleContext };
