a371126cd06525f08acdf80c2224f8bb4623cb4d
RosaBen <benchabane.rosa@outlook.com>
2026-09-03 17:09:02 +0200

    added hook useeffectOnUPdate

--- createHooks/src/components/Menu/Menu.jsx
+++ createHooks/src/components/Menu/Menu.jsx
@@ -3,7 +3,10 @@ import useToggle from "../../hooks/useToggle";
 
 const MenuContext = createContext();
 export default function Menu({ children, onOpen }) {
-  const [open, toggleOpen] = useToggle(true, onOpen);
+  const [open, toggleOpen] = useToggle({
+    initialValue: true,
+    onToggle: onOpen,
+  });
   return (
     <MenuContext.Provider value={{ open, toggleOpen }}>
       <div className="menu">{children}</div>
